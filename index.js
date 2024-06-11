4d450cd70654c443aaee97be5d3c3749e52e0cc4
Animeshhit <animeshkumbhakarhit2@gmail.com>
2024-06-11 20:52:40 +0530

    cluster system added

--- index.js
+++ index.js
@@ -1,44 +1,80 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cluster = require("cluster");
+const os = require("os");
+
+const numberOfCpus = os.cpus().length;
 
 const connectToDb = require("./db/db");
 const { authRoutes, adminRoutes, productsRoutes } = require("./routes");
 const errorHandler = require("./functions/ErrorHandler");
 const { adminMiddalware } = require("./middalware/adminMiddalware");
 
-const app = express();
-const PORT = process.env.PORT || "8080";
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+  console.log(numberOfCpus);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1", productsRoutes);
-app.use("/api/v1/admin", adminMiddalware, adminRoutes);
-
-app.get("/", (req, res) => {
-  try {
-    res.status(200).json({ message: "all systems are working perfect" });
-  } catch (err) {
-    errorHandler(err, res);
+  for (let i = 0; i < numberOfCpus; i++) {
+    cluster.fork();
   }
-});
 
-connectToDb((connection) => {
-  if (connection) {
-    app.listen(PORT, () => {
-      console.log(
-        `server is running with the connection of database at port ${PORT}`
-      );
-    });
-  } else {
-    console.log(`server can't be start something went wrong`);
-  }
-});
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(`worker ${worker.process.pid} is died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
+  const PORT = process.env.PORT || "8080";
+  app.use(
+    cors({
+      origin: "*",
+    })
+  );
+
+  app.use((req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    next();
+  });
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use("/api/v1/auth", authRoutes);
+  app.use("/api/v1", productsRoutes);
+  app.use("/api/v1/admin", adminMiddalware, adminRoutes);
+
+  app.get("/", (req, res) => {
+    try {
+      res.status(200).json({
+        message: `all systems are working perfect response given by ${process.pid} `,
+      });
+    } catch (err) {
+      errorHandler(err, res);
+    }
+  });
+
+  // 404 handler
+  app.use((req, res, next) => {
+    res.status(404).send({ error: "Not Found" });
+  });
+
+  // Error handling middleware
+  app.use((err, req, res, next) => {
+    console.error(`Error in worker ${process.pid}: ${err.message}`);
+    res.status(500).send({ error: "Internal Server Error" });
+  });
+
+  connectToDb((connection) => {
+    if (connection) {
+      app.listen(PORT, () => {
+        console.log(
+          `server is running with the connection of database at  http://localhost:${PORT}`
+        );
+      });
+    } else {
+      console.log(`database connection error`);
+    }
+  });
+}
