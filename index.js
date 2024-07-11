2553882f06eaf8fb01d5118fbfc7a0fff54075db
Animesh Kumbhakar <128497576+Animeshhit@users.noreply.github.com>
2024-07-11 13:55:28 +0530

    without using cluster

--- index.js
+++ index.js
@@ -10,71 +10,72 @@ const connectToDb = require("./db/db");
 const { authRoutes, adminRoutes, productsRoutes } = require("./routes");
 const errorHandler = require("./functions/ErrorHandler");
 const { adminMiddalware } = require("./middalware/adminMiddalware");
+const { authMiddalware } = require("./middalware/authMiddalware");
 
-if (cluster.isPrimary) {
-  console.log(`Master ${process.pid} is running`);
+// if (cluster.isPrimary) {
+//   console.log(`Master ${process.pid} is running`);
 
-  console.log(numberOfCpus);
+//   console.log(numberOfCpus);
 
-  for (let i = 0; i < numberOfCpus; i++) {
-    cluster.fork();
+//   for (let i = 0; i < numberOfCpus; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.error(`worker ${worker.process.pid} is died. Restarting...`);
+//     cluster.fork();
+//   });
+// } else {
+const app = express();
+const PORT = process.env.PORT || "8080";
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://the-craftly-shop.vercel.app"],
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1", productsRoutes);
+app.use("/api/v1/admin", authMiddalware, adminMiddalware, adminRoutes);
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({
+      message: `all systems are working perfect response given by ${process.pid} `,
+    });
+  } catch (err) {
+    errorHandler(err, res);
   }
+});
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.error(`worker ${worker.process.pid} is died. Restarting...`);
-    cluster.fork();
-  });
-} else {
-  const app = express();
-  const PORT = process.env.PORT || "8080";
-  app.use(
-    cors({
-      origin: "*",
-    })
-  );
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).send({ error: "Not Found" });
+});
 
-  app.use((req, res, next) => {
-    res.setHeader("Content-Type", "application/json");
-    next();
-  });
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(`Error in worker ${process.pid}: ${err.message}`);
+  res.status(500).send({ error: "Internal Server Error" });
+});
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
-  app.use("/api/v1/auth", authRoutes);
-  app.use("/api/v1", productsRoutes);
-  app.use("/api/v1/admin", adminMiddalware, adminRoutes);
-
-  app.get("/", (req, res) => {
-    try {
-      res.status(200).json({
-        message: `all systems are working perfect response given by ${process.pid} `,
-      });
-    } catch (err) {
-      errorHandler(err, res);
-    }
-  });
-
-  // 404 handler
-  app.use((req, res, next) => {
-    res.status(404).send({ error: "Not Found" });
-  });
-
-  // Error handling middleware
-  app.use((err, req, res, next) => {
-    console.error(`Error in worker ${process.pid}: ${err.message}`);
-    res.status(500).send({ error: "Internal Server Error" });
-  });
-
-  connectToDb((connection) => {
-    if (connection) {
-      app.listen(PORT, () => {
-        console.log(
-          `server is running with the connection of database at  http://localhost:${PORT}`
-        );
-      });
-    } else {
-      console.log(`database connection error`);
-    }
-  });
-}
+connectToDb((connection) => {
+  if (connection) {
+    app.listen(PORT, () => {
+      console.log(
+        `server is running with the connection of database at  http://localhost:${PORT}`
+      );
+    });
+  } else {
+    console.log(`database connection error`);
+  }
+});
+// }
