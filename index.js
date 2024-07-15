24c7ba856994a1b742fa926deac9fba108b0711b
Animeshhit <animeshkumbhakarhit2@gmail.com>
2024-07-15 19:30:40 +0530

    cors

--- index.js
+++ index.js
@@ -30,10 +30,15 @@ const app = express();
 const PORT = process.env.PORT || "8080";
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://the-craftly-shop.vercel.app",
+      "https://thecraftly.shop",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
   })
 );
-// ["http://localhost:5173", "https://the-craftly-shop.vercel.app","https://thecraftly.shop"],
 app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   next();
